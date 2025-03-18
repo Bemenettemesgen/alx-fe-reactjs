c4d3799ca31e575dad9f52f36c1d957dbff08b48
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-03-18 16:07:36 +0200

    fixed

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,20 +1,20 @@
-import WelcomeMessage from './WelcomeMessage
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <WelcomeMessage />
         <>
+            <WelcomeMessage /> {/* First instance of WelcomeMessage */}
             <div>
-                <a href="https://vite.dev" target="_blank">
+                <a href="https://vite.dev" target="_blank" rel="noreferrer">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
-                <a href="https://react.dev" target="_blank">
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
@@ -30,8 +30,8 @@ function App() {
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
+            <WelcomeMessage /> {/* Second instance of WelcomeMessage */}
         </>
-        <WelcomeMessage />
     );
 }
 
