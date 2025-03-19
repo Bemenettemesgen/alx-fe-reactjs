565c5d5461f7d630bcd76f71e872961d726cedf4
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-03-19 09:57:30 +0200

    Update App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,7 +1,9 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-
+import Header from",/components/Header';
+import MainContent from", /components/MainContent';
+import Footer from",/components/Footer';
 function App() {
     return (
         <div>
