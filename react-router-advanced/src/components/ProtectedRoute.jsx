e7c8c37e5de713f8085e529cc1700b59ca227f1f
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-03-20 16:03:16 +0200

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-    // Simulated authentication check
+ const useAuth = () => {
+        // Simulate authentication by checking for a token in localStorage
+        const token = localStorage.getItem("authToken");
+        return !!token; // Returns true if authenticated, false otherwise
+    };
     const isAuthenticated = localStorage.getItem("authToken");
 
     // If the user is authenticated, render the children components
     return isAuthenticated ? children : <Navigate to="/" />;
+    export default useAuth;
 };
-
-export default ProtectedRoute;
