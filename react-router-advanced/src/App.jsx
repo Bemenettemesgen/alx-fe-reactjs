b55694b9d52ead0fcfe4bcc691fc9b32bb920e0b
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-03-20 15:47:19 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import BlogPost from "./components/BlogPost";
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                {/* Protect the Profile route */}
-                <Route
-                    path="/profile"
-                    element={
-                        <ProtectedRoute>
-                            <Profile />
-                        </ProtectedRoute>
-                    }
-                />
+                <Route path="/profile" element={<Profile />}>
+                    <Route path="details" element={<ProfileDetails />} />
+                    <Route path="settings" element={<ProfileSettings />} />
+                </Route>
+                <Route path="/blog/:id" element={<BlogPost />} />
             </Routes>
         </Router>
     );
