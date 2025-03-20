7e8192ef5c92368f4f7b0a5633afc78694268d83
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-03-20 13:57:35 +0200

    Update App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
 import RegistrationForm from "./components/RegistrationForm";
+import React from "react";
+import FormikForm from "./components/FormikForm";
 
 const App = () => {
     return (
         <div>
             <h1>User Registration</h1>
             <RegistrationForm />
+            <FormikForm />
         </div>
     );
 };
