090e1eeff6ffaea6e0bca372dc38bc11539c5a9c
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-03-20 15:45:48 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
-import Login from "./components/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                {/* Public route: Login */}
-                <Route path="/" element={<Login />} />
-
-                {/* Protected route: Profile */}
+                {/* Protect the Profile route */}
                 <Route
                     path="/profile"
                     element={
