76f84aa770fb8641804ba27962a761a4c74b335a
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-03-29 12:31:50 +0200

    Styling, States, Rendering, Routes and Hooks

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', fontFamily: 'Arial', lineHeight: '1.6', backgroundColor: '#f0f8ff' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
