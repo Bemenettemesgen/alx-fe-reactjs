5e4fcb116c4c227d6d9f9aaa95bed72ff6766c86
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-03-31 21:09:31 +0300

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <nav style={{ backgroundColor: '#eee', padding: '10px' }}>
-            <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
-            <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
-            <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
-            <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
+        <nav style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', backgroundColor: '#eee', padding: '10px' }}>
+            <Link to="/" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Home</Link>
+            <Link to="/about" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>About</Link>
+            <Link to="/services" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Services</Link>
+            <Link to="/contact" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Contact</Link>
         </nav>
     );
 }
