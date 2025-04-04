d41134cfb0117dbf6d592c57f36b3f6a47fd49e7
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-04-04 16:55:43 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -7,18 +7,21 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails'; // Import RecipeDetails
 
-
 function App() {
     return (
         <Router>
             <div>
                 <h1>Recipe Sharing App</h1>
-                <SearchBar />
                 <AddRecipeForm />
                 <RecipeList />
+                <Routes>
+                    <Route path="/" element={<RecipeList />} />
+                    <Route path="/recipe/:recipeId" element={<RecipeDetails />} /> {/* Route for recipe details */}
+                </Routes>
             </div>
         </Router>
     );
 }
 
 export default App;
+
