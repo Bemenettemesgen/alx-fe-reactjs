60d58afbc3c3c4e21486b50e78de81b5343d07ee
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-04-04 16:50:39 +0300

    Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
 
 function App() {
     return (
         <Router>
             <div>
                 <h1>Recipe Sharing App</h1>
+                <SearchBar />
                 <AddRecipeForm />
                 <RecipeList />
-                <Routes>
-                    <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-                </Routes>
             </div>
         </Router>
     );
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,20 +2,17 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
     recipes: [],
-    addRecipe: (newRecipe) =>
-        set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+    searchTerm: '',
+    filteredRecipes: [],
 
-    deleteRecipe: (recipeId) =>
-        set((state) => ({
-            recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
-        })),
-
-    updateRecipe: (recipeId, updatedRecipe) =>
-        set((state) => ({
-            recipes: state.recipes.map((recipe) =>
-                recipe.id === recipeId ? { ...recipe, ...updatedRecipe } : recipe
-            ),
-        })),
+    setSearchTerm: (term) =>
+        set({ searchTerm: term }, () => {
+            set((state) => ({
+                filteredRecipes: state.recipes.filter((recipe) =>
+                    recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+                ),
+            }));
+        }),
 }));
 
 export default useRecipeStore;
