1b95d0ea9888c2ef3735b8dc6b4fab2057706086
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-04-04 17:27:36 +0300

    Update TodoList.test.js

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import TodoList from "../components/TodoList";
-
+test("renders the TodoList component", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Todo List")).toBeInTheDocument();
+});
 test("renders the TodoList component", () => {
     render(<TodoList />);
     expect(screen.getByText("Todo List")).toBeInTheDocument();
