08f5be28f5e3df1935af5a84d18287182427745c
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-04-06 15:37:32 +0300

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -7,4 +7,13 @@ module.exports = {
     darkMode: "media", // Enables dark mode (options: 'media' or 'class')
     plugins: [],
   };
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Replaces the old `purge`
+    theme: {
+      extend: {}, // Customize styles here
+    },
+    darkMode: "media",
+    plugins: [],
+  };
   
