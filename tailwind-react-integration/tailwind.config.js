a996335c577a98749c584ce6af220a72dd390121
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-04-06 15:33:45 +0300

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Scans files for Tailwind utility classes
     theme: {
-      extend: {},
+      extend: {}, // Customize styles here
     },
+    darkMode: "media", // Enables dark mode (options: 'media' or 'class')
     plugins: [],
   };
   
