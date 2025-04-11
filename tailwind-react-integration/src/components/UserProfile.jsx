efb62970c241e385ba5522ea6949a191c4ef4590
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-04-11 09:04:49 +0300

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,6 +5,7 @@ function UserProfile() {
         "sm:w-24", "sm:h-24"
         "hover:scale-110", "transition-transform", "duration-300", "ease-in-out"
         "hover:text-blue-500"
+        "hover:shadow-xl"
         
         <img
           src="https://via.placeholder.com/150"
