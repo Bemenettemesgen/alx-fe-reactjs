184f3731927cc985ab848787dbf0d9a9347717c1
Bemenettemesgen <bemenettemesgen44@gmail.com>
2025-04-11 08:25:21 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,17 +4,8 @@ import axios from 'axios';
 export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data; // Returns user data
+    return response.data; // Return user data on success
   } catch (error) {
-    throw new Error('User not found or API error');
-  }
-};
-
-export const fetchAdvancedUsers = async (query) => {
-  try {
-    const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
-    return response.data.items; // Returns array of users
-  } catch (error) {
-    throw new Error('Error fetching users');
+    throw new Error("User not found or API error");
   }
 };
